7025169970daf71e7890770a8536750cc322a7ac
whtalx <whtalx@gmail.com>
2019-03-24 01:39:47 +0300

    this is commit you looking for

--- src/Snek/Display/__frame/index.js
+++ src/Snek/Display/__frame/index.js
@@ -1,23 +1,26 @@
-import React from 'react'
+import React, {Component} from 'react'
 import './index.css'
 import Pixel from '../__pixel'
+//TODO: сделать метод с setState и экспортировать
+//зачем хранить массив в state?
+class Frame extends Component {
+  render() {
+    let i = 0
+    let grid = []
 
-function Frame() {
-  let i = 0
-  let grid = []
-
-  for (let y = 0; y < 20; y++) {
-    for (let x = 0; x < 10; x++) {
-      grid[i] = <Pixel key = {i} coordinates = {{'x' : x, 'y' : y}} />
-      i++
+    for (let y = 0; y < 20; y++) {
+      for (let x = 0; x < 10; x++) {
+        grid[i] = <Pixel key = {i} x = {x} y = {y} status = 'off' />
+        i++
+      }
     }
+    
+    return (
+      <div className = 'display__frame'>
+      {grid}
+      </div>
+    )
   }
-  
-  return (
-    <div className = 'display__frame'>
-    {grid}
-    </div>
-  )
 }
 
 export default Frame
--- src/Snek/Display/__pixel/index.js
+++ src/Snek/Display/__pixel/index.js
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, {Component} from 'react'
 import './index.css'
-
-function Pixel (props) {
-  return (
-    <div className = 'display__pixel display__pixel_off' x = {props.coordinates.x} y = {props.coordinates.y}>
-      <svg viewBox = '0 0 11 11'>
-        <path style = {{'fillRule': 'evenodd'}} d = 'M1,1h9v9H1V1ZM2,2H9V9H2V2ZM3,3H8V8H3V3Z' />
-      </svg>
-    </div>
-  )
+//TODO: передать возможность setState родителю
+//честно, хз как. мне такое сложно
+class Pixel extends Component {
+  state = {
+      status: this.props.status,
+      x: this.props.x,
+      y: this.props.y
+  }
+  makeBlink() {
+    this.setState({status: 'blink'})
+  }
+  render () {
+    return (
+      <div className = {'display__pixel display__pixel_' + this.state.status} x = {this.state.x} y = {this.state.y}>
+        <svg viewBox = '0 0 11 11'>
+          <path style = {{'fillRule': 'evenodd'}} d = 'M1,1h9v9H1V1ZM2,2H9V9H2V2ZM3,3H8V8H3V3Z' />
+        </svg>
+      </div>
+    )
+  }
 }
 
 export default Pixel
