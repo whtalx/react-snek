672125adf0c4d216c6e5dbeb00541ee2c8ba40c9
whtalx <whtalx@gmail.com>
2019-03-23 19:23:33 +0300

    Updates

--- precache-manifest.61a153cf8c56372c9316195ed9445263.js
+++ precache-manifest.61a153cf8c56372c9316195ed9445263.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "b2dfd7bbb9f1c24bef9e",
-    "url": "/rect-snek/static/css/main.dd83656b.chunk.css"
+    "revision": "6940d82aa8b559044556",
+    "url": "/rect-snek/static/css/main.3e3699c9.chunk.css"
   },
   {
-    "revision": "b2dfd7bbb9f1c24bef9e",
-    "url": "/rect-snek/static/js/main.8869e990.chunk.js"
+    "revision": "6940d82aa8b559044556",
+    "url": "/rect-snek/static/js/main.524d69d7.chunk.js"
   },
   {
     "revision": "dcdf937c2b133e9b45c7",
@@ -28,12 +28,12 @@ self.__precacheManifest = [
     "url": "/rect-snek/static/media/DS-Digital-iefix.37c8019a.eot"
   },
   {
-    "revision": "cad634a7201a7672b9d9e739dc4dd5b6",
-    "url": "/rect-snek/static/media/DS-Digital.cad634a7.svg"
-  },
-  {
     "revision": "223f224650510797e0f06233d2a0f97b",
     "url": "/rect-snek/static/media/DS-Digital.223f2246.ttf"
+  },
+  {
+    "revision": "cad634a7201a7672b9d9e739dc4dd5b6",
+    "url": "/rect-snek/static/media/DS-Digital.cad634a7.svg"
   },
   {
     "revision": "9a918d49406f5d3a79467ec16a5f355e",
@@ -44,7 +44,7 @@ self.__precacheManifest = [
     "url": "/rect-snek/static/media/arrows.88ec1662.svg"
   },
   {
-    "revision": "b9e7560266494b75bce8593df394afef",
+    "revision": "59375847fa9aca9f76048eb91f94563c",
     "url": "/rect-snek/index.html"
   }
 ];
