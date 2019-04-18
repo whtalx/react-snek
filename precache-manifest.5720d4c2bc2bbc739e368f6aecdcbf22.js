3d08da41a529841436dec8cbe01774aec9d873f5
whtalx <whtalx@gmail.com>
2019-04-18 18:52:51 +0300

    Updates

--- precache-manifest.5720d4c2bc2bbc739e368f6aecdcbf22.js
+++ precache-manifest.5720d4c2bc2bbc739e368f6aecdcbf22.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = [
   {
-    "revision": "51640b64d202319d1d42",
-    "url": "/react-snek/static/css/main.cb24c875.chunk.css"
+    "revision": "93a1a19e0533ac60b2ff",
+    "url": "/react-snek/static/css/main.1f0fd519.chunk.css"
   },
   {
-    "revision": "51640b64d202319d1d42",
-    "url": "/react-snek/static/js/main.d1a527a3.chunk.js"
+    "revision": "93a1a19e0533ac60b2ff",
+    "url": "/react-snek/static/js/main.62860c94.chunk.js"
   },
   {
     "revision": "e037d10deb72bb61cc55",
     "url": "/react-snek/static/js/runtime~main.aeda6471.js"
   },
   {
-    "revision": "73ae0dea9640bd7e0ab5",
-    "url": "/react-snek/static/js/2.dc8ed97e.chunk.js"
+    "revision": "7e6f6c8aec9a0c63c56b",
+    "url": "/react-snek/static/js/2.ce520361.chunk.js"
   },
   {
     "revision": "ce409a4dbe4918929313e5a3f24d3e35",
@@ -24,12 +24,12 @@ self.__precacheManifest = [
     "url": "/react-snek/static/media/DS-Digital.6ea2dc63.woff"
   },
   {
-    "revision": "37c8019ad30f65d06d99a40169ae5e46",
-    "url": "/react-snek/static/media/DS-Digital.37c8019a.eot"
-  },
-  {
     "revision": "223f224650510797e0f06233d2a0f97b",
     "url": "/react-snek/static/media/DS-Digital.223f2246.ttf"
+  },
+  {
+    "revision": "37c8019ad30f65d06d99a40169ae5e46",
+    "url": "/react-snek/static/media/DS-Digital.37c8019a.eot"
   },
   {
     "revision": "5b57f90889f55ea8034dff16423bc835",
@@ -40,11 +40,11 @@ self.__precacheManifest = [
     "url": "/react-snek/static/media/legend.9a918d49.svg"
   },
   {
-    "revision": "88ec1662697936c69ef411034c76fda8",
-    "url": "/react-snek/static/media/arrows.88ec1662.svg"
+    "revision": "9f7df41bc848e2c5578a6b8d496d502c",
+    "url": "/react-snek/static/media/arrows.9f7df41b.svg"
   },
   {
-    "revision": "4a67ed532e3d1317be5a04220b61c7b8",
+    "revision": "96e4db9178f4fc58a6aaa410daef58c7",
     "url": "/react-snek/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-snek/precache-manifest.10026117f194203db22e8b65dd106de4.js"
+  "/react-snek/precache-manifest.5720d4c2bc2bbc739e368f6aecdcbf22.js"
 );
 
 workbox.clientsClaim();
